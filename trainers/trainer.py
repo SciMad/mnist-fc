ab8c52cb2529a2b2e95cd0fcfcf123ec097da6df
scimad <scimad2.71@gmail.com>
2019-01-11 14:27:50 +0545

    Implement train_step & actual training

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -1,3 +1,4 @@
+import sys
 from base.base_train import BaseTrain
 import tensorflow as tf
 from models.model import MNIST_Model
@@ -16,7 +17,18 @@ class MNISTTrainer(BaseTrain):
        -loop on the number of iterations in the config and call the train step
        -add any summaries you want using the summary
         """
-        pass
+
+        self.iterator = self.data.load('train')
+        self.sess.run(self.iterator.initializer)
+        self.next_element = self.iterator.get_next()
+
+        i = 0
+        while i<10000:
+            i += 1
+            if i%100 ==0: print("----------On Step:", i, "---------")
+            self.train_step()
+
+
 
     def train_step(self):
         """
@@ -24,8 +36,26 @@ class MNISTTrainer(BaseTrain):
        - run the tensorflow session
        - return any metrics you need to summarize
        """
-        pass
+        batch_data_point = self.sess.run(self.next_element)
+        batch_images = batch_data_point[0]
+        batch_labels = batch_data_point[1]
 
+        batch_onehot_labels = self.sess.run(
+            tf.one_hot(batch_labels,self.config["num_classes"]))
+
+        # print("batch_labels", batch_labels)
+        # print("batch_onehot_labels", batch_onehot_labels)
+
+        loss, _optimiszation = self.sess.run([self.model.loss, self.model.optimization],
+                                     feed_dict={
+                                         self.model.X: batch_images,
+                                         self.model.onehot_labels:
+                                             batch_onehot_labels,
+                                         self.model.learning_rate:
+                                             self.config['learning_rate']
+                                     })
+
+        print(loss)
 
 if __name__ == '__main__':
 
@@ -33,4 +63,7 @@ if __name__ == '__main__':
     data_gen = DataGenerator(config)
     iterator = data_gen.load('train')
     with tf.Session() as sess:
-        mnist_trainer = MNISTTrainer(sess, model, data_gen, config,None)
+        mnist_trainer = MNISTTrainer(sess, model, data_gen, config, None)
+        mnist_trainer.train_epoch()
+
+
